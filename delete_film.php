4976a72d5c6a34acce0949f73e156d6d51d91184
Ariel2711 <arielnaviandanaputra@gmail.com>
2024-10-17 13:14:09 +0700

    update delete image from folder images

--- delete_film.php
+++ delete_film.php
@@ -2,20 +2,27 @@
 include 'db.php';
 
 if (isset($_GET['id_film'])) {
-    $stmt = $conn->prepare("DELETE FROM films WHERE id_film = ?");
-    $stmt->bind_param("i", $_GET['id_film']);
+    $stmt = $conn->prepare("SELECT poster_image FROM films WHERE id_film = ?");
+    $stmt->bind_param("i", $_GET["id_film"]);
+    $stmt->execute();
+    $stmt->bind_result($imagePath);
+    $stmt->fetch();
+    $stmt->close();
 
+    $stmt = $conn->prepare("DELETE FROM films WHERE id_film = ?");
+    $stmt->bind_param("i", $_GET["id_film"]);
 
     if ($stmt->execute()) {
+        if ($imagePath && file_exists($imagePath)) {
+            unlink($imagePath);
+        }
         header("Location: index.php");
         exit();
     } else {
         header("Location: index.php");
     }
 
-
     $stmt->close();
 }
-
 
 $conn->close();
